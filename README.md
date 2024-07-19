77479d4cdb52035caa4d59733c4328688b8f5f39
jjmchew <jjmchew@gmail.com>
2024-07-19 12:02:40 -0600

    feat(api): add upload files, then s3

---
+++ README.md
@@ -0,0 +1,5 @@
+# EC2 code for AWS db interface
+- this code is intended to be deployed on an EC2 instance to facilitate connections to AWS dbs
+- some of it will be tested locally
+
+
---
+++ server.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+import shutil
+import os
+import use_s3
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+
+
+@app.get('/')
+async def root():
+    return {"message": "Server running"}
+
+
+
+
+
+@app.post('/api/upload')
+async def upload(file: UploadFile=File(...)):
+    FILE_DIR = 'tmpfiles'
+
+    # write file to disk
+    if not os.path.exists(f"./{FILE_DIR}"):
+        os.makedirs(f"./{FILE_DIR}")
+
+    file_location = f"./{FILE_DIR}/{file.filename}"
+    with open(file_location, "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)
+
+    use_s3.ul_file(file.filename, dir=FILE_DIR)
+
+    return {"message": f"{file.filename} received"}
+
+
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
