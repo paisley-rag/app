2fd40847c2866bbff1a648388db34af7ed4f9905
Ubuntu <ubuntu@ip-172-31-81-210.ec2.internal>
2024-07-21 07:53:34 +0000

    fix: working RAGAs evals on EC2 instance

--- evals.py
+++ evals.py
@@ -1,4 +1,3 @@
-
 # RAGAs dependencies
 from datasets import Dataset 
 import os
@@ -16,6 +15,22 @@ from ragas.metrics import answer_relevancy
 
 import psycopg2
 
+def connect_to_db():
+    try:
+        conn = psycopg2.connect(
+            user=os.getenv("PG_USER"),
+            password=os.getenv("PG_PASSWORD"),
+            host=os.getenv("PG_HOST", "127.0.0.1"),
+            port=os.getenv("PG_PORT", "5432"),
+            database=os.getenv("PG_DATABASE", "paisley_evals")
+        )
+        print("Connection to the database has been established.")
+        return conn
+    except Exception as e:
+        print("An error occurred while connecting to the database:", e)
+        return None
+
+
 def run_evals(query=None, output=None, context=None):
     data_samples = {
         'question': [query],
@@ -32,31 +47,28 @@ def run_evals(query=None, output=None, context=None):
     store_to_db(query, output, context, answer_relevancy_score)
 
 def store_to_db(query, output, context, answer_relevancy_score):
-    conn = psycopg2.connect(
-        user=os.getenv("PG_USER"),
-        password=os.getenv("PG_PASSWORD"),
-        host="127.0.0.1",
-        port="5432",
-        database="evals"
-    )
+    conn = connect_to_db()
+    if conn is None:
+        return
     cursor = conn.cursor()
     insert_query = """
     INSERT INTO evals (query, output, context, answer_relevancy_score)
     VALUES (%s, %s, %s, %s)
     """
+
+    cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'")
+    tables = cursor.fetchall()
+    print("Existing tables in the database:", [table[0] for table in tables])
+    
     cursor.execute(insert_query, (query, output, context, answer_relevancy_score))
     conn.commit()
     cursor.close()
     conn.close()
 
 def get_evals():
-    conn = psycopg2.connect(
-        user=os.getenv("PG_USER"),
-        password=os.getenv("PG_PASSWORD"),
-        host="127.0.0.1",
-        port="5432",
-        database="evals"
-    )
+    conn = connect_to_db()
+    if conn is None:
+        return
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM evals")
     records = cursor.fetchall()
