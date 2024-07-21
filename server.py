eb14918d4383803bbb4f844a041fa9b3975a3709
jjmchew <jjmchew@gmail.com>
2024-07-20 19:02:59 -0600

    fix(server): invoke server as python file

--- server.py
+++ server.py
@@ -44,6 +44,6 @@ async def upload(file: UploadFile=File(...)):
 
 
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
