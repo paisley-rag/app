456c66de105a7cff26659feefad52569c23e7c38
jjmchew <jjmchew@gmail.com>
2024-07-22 12:45:52 -0600

    fix: updates to setup script for RDS

--- setup_scripts/setup_postgres.sh
+++ setup_scripts/setup_postgres.sh
@@ -3,7 +3,12 @@
 # Install postgresql
 pipenv run sudo apt install postgresql postgresql-contrib
 
-# Switch to the postgres user and run psql commands
-sudo -u postgres psql << init_pg.sql
+# load db details from .env
+set -a
+. ../.env
+set +a
+
+# run init_pg.sql file as admin
+psql --host=$PG_HOST --port=$PG_PORT --username=$PG_ADMIN --password --dbname=postgres -f init_pg.sql
 
 echo "PostgreSQL setup completed successfully."
--- setup_scripts/test_pg.sh
+++ setup_scripts/test_pg.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 set -a
-. ./.env
+. ../.env
 set +a
 
 # Note:  --password will PROMPT the user to enter a password to login
 #           - check .env file for the password
 
-psql --host=$HOST --port=$PORT --username=$USERNAME --password --dbname=$DBNAME
+psql --host=$PG_HOST --port=$PG_PORT --username=$PG_ADMIN --password --dbname=postgres
