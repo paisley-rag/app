509ab8e0616caa92f383dc87ff85badea59c3abb
tlane25 <lanetlane1@gmail.com>
2024-07-24 14:30:36 -0400

    update: named env vars for docdb name and collection

--- lp_ingest.py
+++ lp_ingest.py
@@ -14,14 +14,19 @@ from llama_index.core import StorageContext
 load_dotenv()
 nest_asyncio.apply()
 
+llama_cloud_api_key = os.environ["LLAMA_CLOUD_API_KEY"]
+openai_api_key = os.environ["OPENAI_API_KEY"]
+
 mongo_uri = os.environ["MONGO_URI"]
 mongodb_client = pymongo.MongoClient(mongo_uri)
-store = AWSDocDbVectorStore(mongodb_client, db_name='testdb', collection_name='testcollection')
+docdb_name = os.environ["DOCDB_NAME"]
+docdb_collection = os.environ["DOCDB_COLLECTION"]
+store = AWSDocDbVectorStore(mongodb_client, db_name=docdb_name, collection_name=docdb_collection)
 
 def send_file_to_llama_parse(file_path):
     print("send_file_to_llama_parse")
     parser = LlamaParse(
-        api_key=os.environ["LLAMA_PARSE_API_KEY"],
+        api_key=llama_cloud_api_key,
         result_type="markdown"
     )
 
@@ -29,7 +34,7 @@ def send_file_to_llama_parse(file_path):
 
     print("response received from llama_parse")
     print(markdown_documents[0])
-    input("Press Enter to continue...")
+   
     return markdown_documents
 
 
@@ -38,20 +43,20 @@ def send_file_to_llama_parse(file_path):
 def markdown_to_node(documents):
     
     markdown_parser = MarkdownElementNodeParser(
-        llm=OpenAI(api_key=os.environ["OPENAI_API_KEY"], model="gpt-3.5-turbo"),
+        llm=OpenAI(api_key=openai_api_key, model="gpt-3.5-turbo"),
         num_workers=8,
     )
 
     nodes = markdown_parser.get_nodes_from_documents(documents)
     print('response from markdown_parser')
     print(nodes[0])
-    input("Press Enter to continue...")
+
     return nodes
 
 # convert nodes to vector store
 # side effect: save index to docdb
 def nodes_to_vector_store(nodes):
-    embed_model = OpenAIEmbedding(api_key=os.environ["OPENAI_API_KEY"], model="text-embedding-ada-002")
+    embed_model = OpenAIEmbedding(api_key=openai_api_key, model="text-embedding-ada-002")
     storage_context = StorageContext.from_defaults(vector_store=store)
     index = VectorStoreIndex(nodes, embed_model=embed_model, storage_context=storage_context)
 
