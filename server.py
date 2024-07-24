8678d32fd1495d0fd49c928c5c27e484d31d2bc9
jjmchew <jjmchew@gmail.com>
2024-07-24 09:27:57 -0600

    fix(server.py): for update to SII branch

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import shutil
 import os
 import use_s3
 import load_vectors
+import ingest_file
 import evals
 
 app = FastAPI()
@@ -18,18 +19,14 @@ app.add_middleware(
     allow_headers=['*']
 )
 
-
-
 @app.get('/api')
 async def root():
     return {"message": "Server running"}
-
 
 @app.get('/api/evals')
 async def get_evals():
     eval_table = evals.get_evals()
     return {"message": eval_table}
-
 
 @app.post('/api/upload')
 async def upload(file: UploadFile=File(...)):
@@ -44,6 +41,7 @@ async def upload(file: UploadFile=File(...)):
         shutil.copyfileobj(file.file, file_object)
 
     use_s3.ul_file(file.filename, dir=FILE_DIR)
+    ingest_file.ingest_file_to_docdb(file_location)
 
     return {"message": f"{file.filename} received"}
 
