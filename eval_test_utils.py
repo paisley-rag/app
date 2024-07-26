d6a09483ba7d0e9ec22c9f71cffdec6a62ddc9e2
Ben Boinay <117600738+bboinay@users.noreply.github.com>
2024-07-25 23:45:26 -0400

    chore(evals): create eval util files

---
+++ eval_test_utils.py
@@ -0,0 +1,30 @@
+import requests
+
+import app_logger as log
+
+def mock_query(string):
+    url = "http://52.4.226.198/api/query"
+    payload = {"query": string}  # Replace with actual payload data
+    try:
+        response = requests.post(url, json=payload)
+        if response.status_code == 200:
+            log.info("Request was successful")
+            # print("Response data:", response.json())
+            log.debug('MOCK QUERY RESPONSE OBJECT KEYS:', response.json().keys())
+            return response.json()['body']
+        else:
+            log.info(f"Request failed with status code: {response.status_code}")
+    except Exception as e:
+        log.info(f"An error occurred: {e}")
+
+
+def extract_from_mock_query_response(response):
+    # creates context using 'text' from each 'node' in response's 'source_nodes'. also deletes any instances of '\n'
+    log.debug('RESPONSE IS:', response)
+    source_nodes = response['source_nodes']
+    context_list = [source_node['node']['text'].replace('\n', '') for source_node in source_nodes] 
+    context = '\n\n'.join(context_list)
+
+    output = response['response']
+
+    return [context, output]
