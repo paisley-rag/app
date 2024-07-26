6b8ca38d2c664af88dc75855d6056881c9d1ee3e
Ben Boinay <117600738+bboinay@users.noreply.github.com>
2024-07-25 23:20:36 -0400

    chore: update init_pg.sql to reflect new eval db + tables

--- setup_scripts/init_pg.sql
+++ setup_scripts/init_pg.sql
@@ -1,22 +1,42 @@
+-- this file replaces init_pg.sql
+-- we should have two tables, one for on-the-fly input/context/output entries
+-- and one for golden dataset values (with ~5 additional metrics), but to keep 
+-- things easy for now there's a scored and unscored golden dataset table
+
+
 -- Create a new user with a password
 CREATE USER paisley WITH PASSWORD 'paisley_rules';
 
 -- Create a new database
-CREATE DATABASE paisley_evals;
+CREATE DATABASE paisley_evals2;
 
 -- Connect to the new database
 \c paisley_evals
 
--- Create a new table
-CREATE TABLE evals (
+-- Create new tables
+CREATE TABLE running_evals (
     id SERIAL PRIMARY KEY,
-    query TEXT NOT NULL,
-    output TEXT NOT NULL,
-    context TEXT NOT NULL,
-    answer_relevancy_score FLOAT NOT NULL
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data JSONB
+);
+
+CREATE TABLE golden_dataset (
+    id SERIAL PRIMARY KEY,
+    data JSONB
+);
+
+CREATE TABLE scored_golden_dataset (
+    id SERIAL PRIMARY KEY,
+    data JSONB
 );
 
 -- Grant privileges to the new user on the database and table
-GRANT ALL PRIVILEGES ON DATABASE paisley_evals TO paisley;
-GRANT ALL PRIVILEGES ON TABLE evals TO paisley;
-GRANT USAGE, SELECT ON SEQUENCE evals_id_seq TO paisley;
+GRANT ALL PRIVILEGES ON DATABASE paisley_evals2 TO paisley;
+
+GRANT ALL PRIVILEGES ON TABLE running_evals TO paisley;
+GRANT ALL PRIVILEGES ON TABLE golden_dataset TO paisley;
+GRANT ALL PRIVILEGES ON TABLE scored_golden_dataset TO paisley;
+
+GRANT USAGE, SELECT ON SEQUENCE running_evals_id_seq TO paisley;
+GRANT USAGE, SELECT ON SEQUENCE golden_dataset_id_seq TO paisley;
+GRANT USAGE, SELECT ON SEQUENCE scored_golden_dataset_id_seq TO paisley;
