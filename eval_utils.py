c3459737636994b9be9d0ce5830c495d50e5def2
Ben Boinay <117600738+bboinay@users.noreply.github.com>
2024-07-29 17:13:31 -0400

    squash me

--- eval_utils.py
+++ eval_utils.py
@@ -4,23 +4,36 @@ import app_logger as log
 
 def extract_from_response(response):
     # creates context using 'text' from each 'node' in response's 'source_nodes'. also deletes any instances of '\n'
-    # Check if response has a 'body' attribute and update response if it does
-
-    print('RESPONSE IS:', response)
-    print('TYPE OF RESPONSE:', type(response))
-    print('RESPONSE KEYS:', response.keys())
-    # if hasattr(response, 'body'):
-    response = response.body
-    
     log.debug('RESPONSE IS:', response)
     log.debug('RESPONSE TYPE IS:', type(response))
-    source_nodes = response.source_nodes
-    context_list = [source_node.node.text.replace('\n', '') for source_node in source_nodes] 
+
+    source_nodes = response['body']['source_nodes']
+    context_list = [source_node['node']['text'].replace('\n', '') for source_node in source_nodes] 
     context = '\n\n'.join(context_list)
 
-    output = response.response
+    output = response['body']['response']
 
     return [context, output]
+
+# def extract_from_response(response):
+#     # creates context using 'text' from each 'node' in response's 'source_nodes'. also deletes any instances of '\n'
+#     # Check if response has a 'body' attribute and update response if it does
+
+#     print('RESPONSE IS:', response)
+#     print('TYPE OF RESPONSE:', type(response))
+#     print('RESPONSE KEYS:', response.keys())
+#     # if hasattr(response, 'body'):
+#     response = response.body
+    
+#     log.debug('RESPONSE IS:', response)
+#     log.debug('RESPONSE TYPE IS:', type(response))
+#     source_nodes = response.source_nodes
+#     context_list = [source_node.node.text.replace('\n', '') for source_node in source_nodes] 
+#     context = '\n\n'.join(context_list)
+
+#     output = response.response
+
+#     return [context, output]
 
 def change_nans_to_zeros(score):
     # sometimes when the context and answer really don't match RAGAs gives a 'NaN' faithfulness answer score, which I'll replace with 0.0 for now
