368e28af843b1f12e79d1afac7dfe9fcdf574d4d
jjmchew <jjmchew@gmail.com>
2024-07-28 22:09:54 -0600

    feat: added synthesizer and custom prompts demo

--- pipelineTest.py
+++ pipelineTest.py
@@ -2,6 +2,8 @@ from llama_index.core import QueryBundle
 from llama_index.core.postprocessor import SimilarityPostprocessor
 from llama_index.core.postprocessor import LongContextReorder
 from llama_index.postprocessor.colbert_rerank import ColbertRerank
+from llama_index.core import get_response_synthesizer, PromptTemplate
+from llama_index.core.response_synthesizers import ResponseMode
 
 import hybridSearch.search as search
 
@@ -49,3 +51,34 @@ print('='*20)
 print_nodes(nodes_reorder)
 
 
+
+# Response synthesizer
+synth = get_response_synthesizer(
+    response_mode=ResponseMode.SIMPLE_SUMMARIZE
+)
+
+response = synth.synthesize(query, nodes=nodes_reorder)
+print(response)
+
+print('*'*20)
+
+
+# Custom Prompt
+new_prompt = (
+    "Context information is below.\n"
+    "-----------------------------\n"
+    "{context_str}\n"
+    "-----------------------------\n"
+    "Given the context information and not prior knowledge, "
+    "answer the query in French.\n"
+    "Query: {query_str}\n"
+    "Answer: "
+)
+new_template = PromptTemplate(new_prompt)
+
+synth.update_prompts(
+    {"text_qa_template": new_template}
+)
+
+response = synth.synthesize(query, nodes=nodes_reorder)
+print(response)
