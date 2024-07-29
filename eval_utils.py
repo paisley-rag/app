dda58d996c0dad881c99fd37e1cd10197b833b13
Ben Boinay <117600738+bboinay@users.noreply.github.com>
2024-07-29 17:14:29 -0400

    squash me

--- eval_utils.py
+++ eval_utils.py
@@ -4,8 +4,8 @@ import app_logger as log
 
 def extract_from_response(response):
     # creates context using 'text' from each 'node' in response's 'source_nodes'. also deletes any instances of '\n'
-    log.debug('RESPONSE IS:', response)
-    log.debug('RESPONSE TYPE IS:', type(response))
+    print('RESPONSE IS:', response)
+    print('RESPONSE TYPE IS:', type(response))
 
     source_nodes = response['body']['source_nodes']
     context_list = [source_node['node']['text'].replace('\n', '') for source_node in source_nodes] 
