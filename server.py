969b9a11a896c8dfc3c2ccd3e650d14ee77da297
qseudo <57642143+yg-lim@users.noreply.github.com>
2024-08-06 10:57:18 -0400

    chore: remove .DS_Store file

--- server.py
+++ server.py
@@ -27,13 +27,9 @@ CONFIG_PIPELINE_COL = os.environ["CONFIG_PIPELINE_COL"]
 
 app = FastAPI()
 
-origins = [
-    "https://3.84.61.211",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*']
