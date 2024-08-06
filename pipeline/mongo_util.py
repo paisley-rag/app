f4926076bde3dcfff2c21434903abbb9d4e44b14
jjmchew <49774645+jjmchew@users.noreply.github.com>
2024-08-06 13:03:53 -0600

    Pipeline5 (#31)
    * feat: added error method for error class logging
    
    * feat: imports front-end from separate repo
    
    * feat: file upload form via modal
    
    * chore: git fix
    
    * feat: initial implementation of pylint and pytest with sample test file
    
    * chore: clean-up / refactor for pipeline folder
    
    * chore: refactor: use APIRouter in fastapi routes, close mongo connections
    
    * fix: updated to remove .DS_Store and minor linting errors
    
    ---------
    
    Co-authored-by: qseudo <57642143+yg-lim@users.noreply.github.com>

---
+++ pipeline/mongo_util.py
@@ -0,0 +1,41 @@
+import os
+
+import pymongo
+from dotenv import load_dotenv
+# import app_logger as log
+
+load_dotenv(override=True)
+
+mongo_uri = os.environ["MONGO_URI"]
+
+
+def get(db_name, db_collection, query=None, projection=None):
+    mongo = pymongo.MongoClient(mongo_uri)
+    result = mongo[db_name][db_collection].find_one(query, projection)
+    mongo.close()
+    return result
+
+
+# used in /api/chatbots route
+def get_all(db_name, db_collection, query=None, projection=None):
+    mongo = pymongo.MongoClient(mongo_uri)
+    results = mongo[db_name][db_collection].find(query, projection)
+    mongo.close()
+    ar = []
+    for result in results:
+        ar.append(result)
+    return ar
+
+
+def insert_one(db_name, db_collection, doc):
+    mongo = pymongo.MongoClient(mongo_uri)
+    result = mongo[db_name][db_collection].insert_one(doc)
+    mongo.close()
+    return result
+
+
+
+# unused
+# def update(db_name, db_collection, filter=None, newObj=None):
+#     mongo[db_name][db_collection].UpdateOne(filter, newObj)
+#     log.info(f"mongo_util.py update:  {db_name} {db_collection} {filter} {newObj}")
---
+++ routers/chatbots.py
@@ -0,0 +1,76 @@
+import os
+import json
+
+from fastapi import Request, APIRouter
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+import app_logger as log
+import pipeline.mongo_util as mutil
+import pipeline.config_util as cutil
+
+router = APIRouter(
+    prefix='/api/chatbots'
+)
+
+load_dotenv(override=True)
+MONGO_URI = os.environ["MONGO_URI"]
+CONFIG_DB = os.environ["CONFIG_DB"]
+CONFIG_PIPELINE_COL = os.environ["CONFIG_PIPELINE_COL"]
+
+class UserQuery(BaseModel):
+    query: str
+
+class QueryBody(UserQuery):
+    chatbot_id: str
+
+@router.get('/')
+async def get_chatbots():
+    log.info('/api/chatbots loaded')
+    results = mutil.get_all(CONFIG_DB, CONFIG_PIPELINE_COL, {}, { '_id': 0 })
+    log.info('/api/chatbots results:', results)
+    return json.dumps(results)
+
+@router.get('/{id}')
+async def get_chatbots_id(id: str):
+    results = mutil.get(CONFIG_DB, CONFIG_PIPELINE_COL, {"id": id}, {'_id': 0})
+    log.info(f"/api/chatbots/{id}: ", results)
+    if not results:
+        return json.dumps({"message": "no chatbot configuration found"})
+
+    return json.dumps(results)
+
+@router.post('/')
+async def post_chatbots(request: Request):
+    body = await request.json()
+    log.info(f"/api/chatbots POST body: ", body)
+    pipeline_obj = cutil.ui_to_pipeline(json.dumps(body))
+
+    mutil.insert_one(CONFIG_DB, CONFIG_PIPELINE_COL, pipeline_obj)
+    # insert_one seems to include the '_id' property which cannot be serialized by json.dumps
+    del pipeline_obj['_id']
+
+    pipeline_json = json.dumps(pipeline_obj)
+    log.debug(f"/api/chatbots POST: pipeline_obj, pipeline_json", pipeline_obj, pipeline_json)
+    return pipeline_json
+
+
+
+    # JSON Shape from UI
+    # {
+    #       "id": "test1",
+    #       "name": "test1",
+    #       "knowledge_bases": ["giraffes"],
+    #       "generative_model": "gpt-4-o",
+    #       "similarity": {
+    #             "on": "True",
+    #             "cutoff": 0.5
+    #           },
+    #       "colbert_rerank": {
+    #             "on": "True",
+    #             "top_n": 0.4
+    #           },
+    #       "long_contet_reorder": "True",
+    #       "prompt": "hello"
+    # }
+
