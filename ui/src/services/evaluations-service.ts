e052fe2c438fd80c6dac8c07fc3b968031cd8994
bboinay <117600738+bboinay@users.noreply.github.com>
2024-08-07 17:08:54 -0400

    Update evaluations-service.ts

--- ui/src/services/evaluations-service.ts
+++ ui/src/services/evaluations-service.ts
@@ -7,8 +7,9 @@ async function fetchChatbotHistory() {
   const response = await axios.get(`${baseUrl}/api/history`);
   console.log('RESPONSE IS:', response)
   console.log('RESPONSE.DATA IS:', response.data)
-
-  return response.data;
+  console.log('RESPONSE.DATA[TABLE_DATA] IS:', response.data['table_data'])
+  
+  return response.data['table_data'];
 }
 
 export const evaluationsService = {
