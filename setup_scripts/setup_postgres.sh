95d76726bdf9501c0ecef642969e7c104220538d
Ben Boinay <117600738+bboinay@users.noreply.github.com>
2024-08-26 18:38:49 -0400

    merge fix

--- setup_scripts/setup_postgres.sh
+++ setup_scripts/setup_postgres.sh
@@ -11,13 +11,10 @@ pipenv run sudo apt install postgresql postgresql-contrib
 # load db details from .env
 set -a
 . ~/db/.env
-. ~/db/.env
 set +a
 
 # run init_pg.sql file as admin
 export PGPASSWORD=$PG_ADMINPW
-echo "pg password is: $PGPASSWORD"
-
 psql --host=$PG_HOST --port=$PG_PORT --username=$PG_ADMIN --dbname=postgres \
   -v pg_database=$PG_DATABASE -v pg_user=$PG_USER -v pg_password=$PG_PASSWORD -f ~/db/setup_scripts/init_pg.sql
 
