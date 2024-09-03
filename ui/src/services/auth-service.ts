da6cdb9255e85fb16be591a6306ec11d01a1f07c
Ben Boinay <117600738+bboinay@users.noreply.github.com>
2024-09-03 13:33:24 -0400

    working auth
        Co-authored-by: yg-lim <57642143+yg-lim@users.noreply.github.com>
        Co-authored-by: tlane25 <163203257+tlane25@users.noreply.github.com>

--- ui/src/services/auth-service.ts
+++ ui/src/services/auth-service.ts
@@ -6,7 +6,7 @@ export async function authenticate(username: string, password: string) {
   formData.append('username', username);
   formData.append('password', password);
 
-  const response = await axiosInstance.post(`${baseUrl}/token`, formData, {
+  const response = await axiosInstance.post(`${baseUrl}/api/token`, formData, {
     headers: {
       'Content-Type': 'multipart/form-data'
     }
