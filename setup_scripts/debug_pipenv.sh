11e4ff1c190eb973ca55050bfd2cd59a539552a0
jjmchew <jjmchew@gmail.com>
2024-09-06 15:13:46 -0600

    fix: updated script

--- setup_scripts/debug_pipenv.sh
+++ setup_scripts/debug_pipenv.sh
@@ -2,5 +2,5 @@
 
 while read -r requirement; do
 	echo "INSTALLING $requirement"
-	pipenv run pip install $requirement --verbose >>/home/ubuntu/setup.log 2>&1
+	cd /home/ubuntu/db && pipenv run pip install $requirement --verbose >>/home/ubuntu/setup.log 2>&1
 done </home/ubuntu/db/requirements.txt
