8a265f9b93ba3ff90d55341cb3989de652a4e07a
jjmchew <jjmchew@gmail.com>
2024-09-06 23:18:51 -0600

    fix: update scripts

--- util/start_server.sh
+++ util/start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export PIPENV_PIPFILE=/home/ubuntu/db/Pipfile
+export WORKON_HOME=/root/.local/share/virtualenvs/db-grdQ2Ybz
 export PYTHONPATH=$(pipenv --venv)/bin
-exec pipenv run python -m db.server
+exec sudo pipenv run python -m db.server
+
