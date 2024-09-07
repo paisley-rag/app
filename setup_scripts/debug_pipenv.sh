7cc82b53aff36dbc697d46c5402377b64e89337e
jjmchew <jjmchew@gmail.com>
2024-09-07 09:01:37 -0600

    fix: update scripts for no pipenv

--- setup_scripts/debug_pipenv.sh
+++ setup_scripts/debug_pipenv.sh
@@ -2,5 +2,5 @@
 
 while read -r requirement; do
 	echo "INSTALLING $requirement"
-	PIPENV_PIPFILE=/home/ubuntu/db/Pipfile pipenv run pip install $requirement --verbose >>/home/ubuntu/setup.log 2>&1
+	pip install $requirement --verbose >>/home/ubuntu/setup.log 2>&1
 done </home/ubuntu/db/requirements.txt
--- util/start_server.sh
+++ util/start_server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 export PIPENV_PIPFILE=/home/ubuntu/db/Pipfile
-export WORKON_HOME=/root/.local/share/virtualenvs/db-grdQ2Ybz
 export PYTHONPATH=$(pipenv --venv)/bin
-exec sudo pipenv run python -m db.server
-
+exec pipenv run python -m db.server
