3e548e303835927afbaaddeac08eb72316ed2d0d
jjmchew <jjmchew@gmail.com>
2024-09-07 17:21:29 -0600

    fix: updated dist

--- ui/src/lib/utils.ts
+++ ui/src/lib/utils.ts
@@ -5,9 +5,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const baseUrl = import.meta.env.VITE_BASE_URL || '';
+export const baseUrl = import.meta.env.BASE_URL || '';
 
-export const PAISLEY_API_KEY = import.meta.env.VITE_PAISLEY_API_KEY;
+// export const PAISLEY_API_KEY = import.meta.env.VITE_PAISLEY_API_KEY;
 
 //  TODO: (maybe) Add API-key for query route use
 export const AXIOS_CONFIG = {
